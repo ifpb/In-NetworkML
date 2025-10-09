0e5e6407689158b2dff4b688ca540fa2d480f181
caio86 <caioluiz86@gmail.com>
2025-10-08 22:23:05 -0300

    update run-scenario script

--- run-scenario.sh
+++ run-scenario.sh
@@ -1,47 +1,57 @@
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCENARIOS_DIR="${SCRIPT_DIR}/scenarios"
+LOGS_DIR="${SCRIPT_DIR}/logs"
+
 saida() {
-  echo "Usage: ./run-scenario.sh [ scenario | duration ]"
-  echo "Available scenarios:"
+  echo "Uso: ./run-scenario.sh [ cenário | duração ]"
+  echo "Cenários disponíveis:"
   ls scenarios
   exit 1
 }
 
-set -euo pipefail
-
 if [ -z $1 ] || [ ! -d scenarios/$1 ]; then
   saida
 fi
+
+SCENARIO="$1"
+SCENARIO_DIR="${SCENARIOS_DIR}/${SCENARIO}"
 
 if [[ -z $2 ]]; then
   DURATION=60
 elif [[ $2 =~ ^[0-9]+$ ]]; then
   DURATION=$2
 else
-  echo "Duration must be a positive number"
+  echo "Duração deve ser um inteiro positivo"
   exit 1
 fi
 
-echo "Rodando cenário $1 com duração de $DURATION"
+echo "Rodando cenário ${SCENARIO} com duração de ${DURATION}s"
+
+mkdir -p ${LOGS_DIR}
 
 vagrant up --provision
-vagrant ssh-config >ssh_config
+vagrant ssh-config >"${SCRIPT_DIR}/ssh_config"
 
-ansible-playbook scenarios/$1/server.yml
-ansible-playbook scenarios/$1/client.yml
+echo "Rodando playbook do servidor"
+ansible-playbook "${SCENARIO_DIR}/server.yml"
+echo "Rodando playbook do servidor"
+ansible-playbook "${SCENARIO_DIR}/client.yml"
 
-mkdir -p logs
-if [[ -e ./scenarios/$1/server.sh ]]; then
+if [[ -f "${SCENARIO_DIR}/server.sh" ]]; then
   echo "Rodando script do servidor"
-  scp -F ./ssh_config ./scenarios/$1/server.sh h2:/tmp
-  ssh -F ./ssh_config h2 'sudo nohup bash /tmp/server.sh >/vagrant/logs/server.log 2>/vagrant/logs/server.err & echo $! > /tmp/server.pid' &
+  scp -F "${SCRIPT_DIR}/ssh_config" "${SCENARIO_DIR}/server.sh" h2:/tmp
+  ssh -F "${SCRIPT_DIR}/ssh_config" h2 'sudo nohup bash /tmp/server.sh >/vagrant/logs/server.log 2>/vagrant/logs/server.err & echo $! > /tmp/server.pid' &
 fi
 
-if [[ -e ./scenarios/$1/client.sh ]]; then
-  echo "Rodando script do client"
-  scp -F ./ssh_config ./scenarios/$1/client.sh h1:/tmp
-  ssh -F ./ssh_config h1 "sudo DURATION=$DURATION bash /tmp/client.sh > /vagrant/logs/client.log 2>/vagrant/logs/client.err"
+if [[ -f "${SCENARIO_DIR}/client.sh" ]]; then
+  echo "Rodando script do client com duração de ${DURATION}s"
+  scp -F "${SCRIPT_DIR}/ssh_config" "${SCENARIO_DIR}/client.sh" h1:/tmp
+  ssh -F "${SCRIPT_DIR}/ssh_config" h1 "sudo DURATION=$DURATION bash /tmp/client.sh > /vagrant/logs/client.log 2>/vagrant/logs/client.err"
 fi
 
-if [[ -e ./scenarios/$1/server.sh ]]; then
+if [[ -f "${SCENARIO_DIR}/server.sh" ]]; then
   echo "Desligando servidor"
-  ssh -F ./ssh_config h2 'kill $(cat /tmp/server.pid)'
+  ssh -F "${SCRIPT_DIR}/ssh_config" h2 'kill $(cat /tmp/server.pid)'
 fi
