f1df16e73f8792618634495b235d948d4711e6b5
caio86 <caioluiz86@gmail.com>
2025-10-29 12:07:36 -0300

    create run-iperf.sh script to automate capturing iperf output to a json file

---
+++ run-iperf.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -u
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+METRICS_DIR="${SCRIPT_DIR}/metrics"
+OUTPUT_FILE="/vagrant/metrics/iperf.json"
+H2_IP="192.168.56.102"
+
+mkdir "${METRICS_DIR}" 2>/dev/null
+
+ssh -F "${SCRIPT_DIR}/ssh_config" h2 "sudo iperf3 -s -J > ${OUTPUT_FILE}" &
+RECEIVER_PID=$!
+
+sleep .2
+
+ssh -F "${SCRIPT_DIR}/ssh_config" h1 "sudo iperf3 -t 0 -c ${H2_IP} >/dev/null 2>&1" &
+SENDER_PID=$!
+
+cleanup() {
+  ssh -F "${SCRIPT_DIR}/ssh_config" h1 "sudo pkill -2 -f iperf3"
+  ssh -F "${SCRIPT_DIR}/ssh_config" h2 "sudo pkill -2 -f iperf3"
+
+  kill $SENDER_PID 2>/dev/null || true
+  kill $RECEIVER_PID 2>/dev/null || true
+
+  wait $SENDER_PID 2>/dev/null
+  wait $RECEIVER_PID 2>/dev/null
+}
+
+trap cleanup INT TERM EXIT KILL
+
+wait $SENDER_PID
