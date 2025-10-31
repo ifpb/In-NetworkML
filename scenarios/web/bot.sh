16859936c71e62a81bc5be6ea5b00d2d7649b0d9
icaroo <icaromachadosilva@gmail.com>
2025-10-30 21:21:25 -0300

    coletando métricas do web server

--- scenarios/web/bot.sh
+++ scenarios/web/bot.sh
@@ -12,13 +12,39 @@ check() {
 	return 1
 }
 
-dfs() {
-	wget "$link""$1"
+get_file() {
 	file=$1
-	[ -z $file ] && file="/pages/index.html"
+	[ -z $file ] && file="index.html"
+	echo "-----SOS-----" >> snapshots
+	echo "Visiting $link/$file" >> snapshots
+	echo "TIMESTAMP=$(date +%s%3N)" >> snapshots
+	curl -w "
+Time Details:
+-------------
+Name Lookup:    %{time_namelookup}s
+Connect:        %{time_connect}s
+Pre-transfer:   %{time_pretransfer}s
+Start-transfer: %{time_starttransfer}s
+Total:          %{time_total}s
+
+Size Details:
+-------------
+Download:       %{size_download} bytes
+Header Size:    %{size_header} bytes
+
+HTTP Code:      %{http_code}
+	" -o $file "$link/$1" >> snapshots
+	echo "-----EOS------
+"	>> snapshots
+}
+
+dfs() {
+	file=$1
+	[ -z $file ] && file="index.html"
 	echo $file >> visited
+	get_file $1
 	# check $href && echo "Visiting $file"
-	for href in $(cat $(echo $file | awk -F "/" '{print $3}') | grep ">a<" | awk -F '"' '{print $2}'); do
+	for href in $(cat $file | grep ">a<" | awk -F '"' '{print $2}'); do
 		check $href && dfs $href
 	done
 	return 0
@@ -29,6 +55,7 @@ if [ -z $1 ]; then
 fi
 
 echo "" > visited
+echo "" > snapshots
 link="http://"$1
 dfs
 clean
