1e39ae13976c058aa5321d817380ec43e8f63083
caio86 <caioluiz86@gmail.com>
2026-01-09 17:33:42 -0300

    edit plot_graphs.py

--- code/plot_graphs.py
+++ code/plot_graphs.py
@@ -1,50 +1,81 @@
 #!/usr/bin/env python3
 
-import json
 import argparse
+import json
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-
 from colors import label_color
 
 
 def extract_iperf(inputfile):
     with open(f"{inputfile}iperf.json") as f:
-        obj = json.load(f)
+        lines = f.readlines()
+        try:
+            obj = json.loads("".join(lines))
+        except json.JSONDecodeError:
+            obj = json.loads("".join(lines[:-11]))
         lst = [x["sum"]["bits_per_second"] / 1e6 for x in obj["intervals"]]
     return lst
 
+
 def plot_iperf(data, path):
-    plt.figure(figsize=(12, 6))
-    #sns.kdeplot(data, cumulative=True, linewidth=2) #label=data.columns)
-    sns.lineplot(data["ML"].rolling(10).mean(), linewidth=2, color=label_color["ml"]["color"], label="ML")
-    sns.lineplot(data["Sem ML"].rolling(10).mean(), linewidth=2, color=label_color["wml"]["color"], label="Sem ML")
-    plt.ylabel('Vazão (mbps)')
-    plt.xlabel('Tempo (s)')
+    # plt.figure(figsize=(12, 6))
+    # sns.kdeplot(
+    #     data["ML"],
+    #     cumulative=True,
+    #     linewidth=2,
+    #     color=label_color["ml"]["color"],
+    #     label="With ML",
+    # )
+    # sns.kdeplot(
+    #     data["Sem ML"],
+    #     cumulative=True,
+    #     linewidth=2,
+    #     color=label_color["wml"]["color"],
+    #     label="Without ML",
+    # )
+    # plt.ylabel("Density")
+    # plt.xlabel("Throughput (mbps)")
+
+    sns.lineplot(
+        data["ML"].rolling(10).mean(),
+        linewidth=2,
+        color=label_color["ml"]["color"],
+        label="With ML",
+    )
+    sns.lineplot(
+        data["Sem ML"].rolling(10).mean(),
+        linewidth=2,
+        color=label_color["wml"]["color"],
+        label="Without ML",
+    )
+    plt.ylabel("Throughput (mbps)")
+    plt.xlabel("Time (s)")
     plt.legend()
     plt.grid(alpha=0.3)
     plt.tight_layout()
-    plt.savefig(f"{path}/bits_per_second.png", dpi=300, bbox_inches='tight')
+    plt.savefig(f"{path}/bits_per_second.png", dpi=300, bbox_inches="tight")
 
 
 def main(args):
     data = pd.DataFrame()
     ml = extract_iperf(args.i[0])
     sml = extract_iperf(args.i[1])
-    print(len(ml), len(sml))
-    data["ML"] = ml
-    data["Sem ML"] = sml
+    # print(len(ml), len(sml))
+    qtd_linhas = min(len(ml), len(sml)) - 1
+    data["ML"] = ml[:qtd_linhas]
+    data["Sem ML"] = sml[:qtd_linhas]
     plot_iperf(data, "./")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-i", nargs="+", required=True, help="Path to metrics")
 
     args = parser.parse_args()
-    
+
     main(args)
