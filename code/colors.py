af2d5049c52acbf53f72c3cf4534525ad6bfd8ad
caio86 <caioluiz86@gmail.com>
2026-01-09 17:28:23 -0300

    create colors.py

---
+++ code/colors.py
@@ -0,0 +1,22 @@
+label_color = {
+    "ml": {
+        "color": "C0",
+        "name": "With ML",
+    },
+    "wml": {
+        "color": "C1",
+        "name": "Without ML",
+    },
+    "0": {
+        "color": "C0",
+        "name": "Web browsing",
+    },
+    "1": {
+        "color": "C1",
+        "name": "File transfer",
+    },
+    "2": {
+        "color": "C2",
+        "name": "Video streaming",
+    },
+}
