30c7bd08ba18cf28d7f1b0262fcfca93ea0be294
caio86 <caioluiz86@gmail.com>
2026-01-12 02:48:48 -0300

    change rolling mean of http metrics graph

--- code/plot_http_metrics.py
+++ code/plot_http_metrics.py
@@ -11,23 +11,23 @@ def shift_timestamp(df, offset):
         df.at[i, 'timestamp'] /= 1e3
 
 def plot_figure(dataset1, dataset2, metric='time_total'):
-    plt.plot(dataset1['timestamp'], dataset1[metric].rolling(10).mean(), label="Without ML", color=label_color["wml"]["color"])
-    plt.plot(dataset2['timestamp'], dataset2[metric].rolling(10).mean(), label="With ML", color=label_color["ml"]["color"])
-    plt.ylabel(metric)
+    plt.plot(dataset1['timestamp'], dataset1[metric].rolling(20).mean(), label="Without ML", color=label_color["wml"]["color"])
+    plt.plot(dataset2['timestamp'], dataset2[metric].rolling(20).mean(), label="With ML", color=label_color["ml"]["color"])
+    plt.ylabel("time_total (seconds)")
     plt.xlabel('time (seconds)')
     #plt.title(f'Http {metric}')
     plt.grid(alpha=0.3)
     plt.legend()
     plt.tight_layout()
     plt.savefig('http_metrics.png')
-    
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset1", help="First dataset to be extracted (NO ML)")
     parser.add_argument("dataset2", help="Second dataset to be extracted (WITH ML)")
     parser.add_argument("-m", "--metric", help="Metric to be compared in both datasets")
-    
+
 
 
     args = parser.parse_args()
